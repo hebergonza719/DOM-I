c36184151e8b998383f3d27b3c2a2050935e03c1
Heber <hebergonza@gmail.com>
2019-10-30 02:30:06 -0400

    finished second task

--- js/index.js
+++ js/index.js
@@ -40,3 +40,56 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let navItems = document.querySelectorAll("nav a");
+for (let i = 0; i < navItems.length; i ++) {
+  navItems[i].textContent = siteContent["nav"][`nav-item-${i + 1}`]
+}
+
+let ctaHOne = document.getElementsByTagName("h1");
+ctaHOne[0].innerHTML = 'DOM<br> Is<br> Awesome';
+
+let ctaImage = document.querySelector("#cta-img");
+ctaImage.setAttribute('src', siteContent["cta"]["img-src"]);
+
+let middleImage = document.querySelector("#middle-img");
+middleImage.setAttribute("src", siteContent["main-content"]["middle-img-src"]);
+
+let buttonMain = document.querySelector(".cta-text button");
+buttonMain.textContent = siteContent["cta"]["button"];
+
+let HFours = document.getElementsByTagName("h4");
+HFours[0].textContent = siteContent["main-content"]["features-h4"];
+
+HFours[1].textContent = siteContent["main-content"]["about-h4"];
+
+HFours[2].textContent = siteContent["main-content"]["services-h4"];
+
+HFours[3].textContent = siteContent["main-content"]["product-h4"];
+
+HFours[4].textContent = siteContent["main-content"]["vision-h4"];
+
+let paragraphs = document.getElementsByTagName("p");
+paragraphs[0].textContent = siteContent["main-content"]["features-content"];
+
+paragraphs[1].textContent = siteContent["main-content"]["about-content"];
+
+paragraphs[2].textContent = siteContent["main-content"]["services-content"];
+
+paragraphs[3].textContent = siteContent["main-content"]["product-content"];
+
+paragraphs[4].textContent = siteContent["main-content"]["vision-content"];
+
+let contactHFour = document.querySelector(".contact h4");
+contactHFour.textContent = siteContent["contact"]["contact-h4"];
+
+let contactParagraphs = document.querySelectorAll(".contact p");
+contactParagraphs[0].textContent = siteContent["contact"]["address"];
+
+contactParagraphs[1].textContent = siteContent["contact"]["phone"]; 
+
+contactParagraphs[2].textContent = siteContent["contact"]["email"];
+
+let footerElem = document.getElementsByTagName("footer");
+let footerText = footerElem[0].getElementsByTagName("p");
+footerText[0].textContent = siteContent["footer"]["copyright"];
